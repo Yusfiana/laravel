f535066de4b608e913e962b9f50457865f023580
Yusfiana <vallanyuz15@gmail.com>
2024-12-11 01:54:25 +0700

    first commit

---
+++ chirper/app/Models/Post.php
@@ -0,0 +1,29 @@
+<?php
+// app/Models/Post.php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'content',
+    ];
+
+    /**
+     * The hashtags that belong to the post.
+     */
+    public function hashtags()
+    {
+        return $this->belongsToMany(Hashtag::class);
+    }
+}
